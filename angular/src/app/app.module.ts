efdf50dd85fbe9d3adfa9d2c19e93f8f21d56917
connectpeoplewithpeople <connectpeoplewithpeople@gmail.com>
2018-04-29 04:10:37 +0900

    Frontend : 네이게이션바 디자인, 메뉴 리스트 추가

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from "@angular/router";
 import { HashLocationStrategy, Location, LocationStrategy } from '@angular/common';
 import { SuiModule } from 'ng2-semantic-ui';
@@ -14,9 +15,11 @@ import { AllianceComponent } from './static/alliance/alliance.component';
 import { RequestComponent } from './static/request/request.component';
 import { PolicyComponent } from './static/policy/policy.component';
 import { PrivacyComponent } from './static/privacy/privacy.component';
+import { IntroComponent } from './static/intro/intro.component';
 
 const appRoutes: Routes = [
   { path: '', component: IndexComponent },
+  { path: 'static/intro', component: IntroComponent },
   { path: 'static/policy', component: PolicyComponent },
   { path: 'static/privacy', component: PrivacyComponent },
   { path: 'static/request', component: RequestComponent },
@@ -34,7 +37,8 @@ const appRoutes: Routes = [
     AllianceComponent,
     RequestComponent,
     PolicyComponent,
-    PrivacyComponent
+    PrivacyComponent,
+    IntroComponent
   ],
   imports: [
     RouterModule.forRoot(
@@ -43,7 +47,8 @@ const appRoutes: Routes = [
     FormsModule,
     SuiModule,
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [
     Location, { provide: LocationStrategy, useClass: HashLocationStrategy }
--- angular/src/app/common/navbar/navbar.component.ts
+++ angular/src/app/common/navbar/navbar.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, Inject } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
+  animations: [
+    trigger('focusSearch', [
+      state('focus', style({
+        width: '180px'
+      })),
+      state('blur', style({
+        width: '120px'
+      })),
+      transition('* => *', animate(100))
+    ]),
+    trigger('scrollOffset', [
+      state('up', style({
+        height: '60px',
+        display: 'block'
+      })),
+      state('down', style({
+        height: '0px',
+        opacity: 0,
+        display: 'none'
+      })),
+      transition('* => *', animate(100))
+    ]),
+  ]
 })
 export class NavbarComponent implements OnInit {
+  baseUrl: string = environment.baseUrl;
+
+  focusSearchState: string = 'blur'; // focus or blur
+  scrollOffsetState: string = 'up'; // up or down
+  prevScrollOffset: number = 0;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  @HostListener("window:scroll", ['$event'])
+  onWindowScroll(event: any) {
+    let scrollOffset: number = window.pageYOffset
+      || document.documentElement.scrollTop
+      || document.body.scrollTop || 0;
+    if (this.prevScrollOffset <= scrollOffset) {
+      this.scrollOffsetState = 'down';
+    } else {
+      this.scrollOffsetState = 'up';
+    }
+    this.prevScrollOffset = scrollOffset;
   }
 
 }
